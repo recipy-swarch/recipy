cd20d053444317a703fb9276bd53147e477c8c48
Juan David <juandavidvasquezpinzon@gmail.com>
2025-06-04 20:21:11 -0500

    [IMGUR_MS]: Cambiado de imgur-api a imgur-ms, además que mejoré el código para incluir UI de swagger, eliminé tests previos, y ahora permite multipart

--- imgur-ms/Program.cs
+++ imgur-ms/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,19 +7,24 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo 
+    { 
+        Title = "imgur-ms API", 
+        Version = "v1" 
+    });
+});
 builder.Services.AddHttpClient();
 
 var app = builder.Build();
 
-if (!app.Environment.IsProduction()) // or check a custom flag
-{
-    app.UseHttpsRedirection();
-}
-
 // Configure the HTTP request pipeline.
 app.UseSwagger();
-app.UseSwaggerUI();
+app.UseSwaggerUI(c =>
+   {
+       c.SwaggerEndpoint("/swagger/v1/swagger.json", "imgur-ms v1");
+   });
 
 app.UseHttpsRedirection();
 

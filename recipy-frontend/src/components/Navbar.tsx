b0f93134b785af3be73ee4d49bc4e9343f7193c8
sergio <saparadaa@unal.edu.co>
2025-06-13 17:52:42 -0500

    cambios en el main y navbar

--- recipy-frontend/src/components/Navbar.tsx
+++ recipy-frontend/src/components/Navbar.tsx
@@ -8,71 +8,31 @@ export default function Navbar() {
 
   return (
     <nav className="navbar bg-body-transparent px-3">
-        <a className="navbar-brand" href="/#">Navbar</a>
-        <ul className="nav nav-pills">
-      {!isLoggedIn ? (
-        <>
-          <li className="nav-item">
-            <button className="btn">
-              <Link className="nav-link" href="/login">Login</Link>
-            </button>
-          </li>
-          <li className="nav-item">
-            <button className="btn special-btn">
-              <Link className="nav-link" href="/register">Register</Link>
-            </button>
-          </li>
-        </>
-      ) : (
-        <>
-          <li className="nav-item">
-            <button className="btn">
-              <Link className="nav-link" href="/create-recipe">Create Recipe</Link>
-            </button>
-          </li>
-          <li className="nav-item">
-            <button className="btn">
-              <Link className="nav-link" href="/profile">My Profile</Link>
-            </button>
-          </li>
-          <li className="nav-item">
-            <button className="btn special-btn" onClick={logout}>
-              Logout
-            </button>
-          </li>
-        </>
-      )}
-    </ul>
-
+      <a className="navbar-brand" href="/#">Navbar</a>
+      <ul className="nav nav-pills">
+        {!isLoggedIn ? (
+          <>
+            <li className="nav-item">
+              <Link className="btn nav-link" href="/login">Login</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="btn special-btn nav-link" href="/register">Register</Link>
+            </li>
+          </>
+        ) : (
+          <>
+            <li className="nav-item">
+              <Link className="btn nav-link" href="/create-recipe">Create Recipe</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="btn nav-link" href="/profile">My Profile</Link>
+            </li>
+            <li className="nav-item">
+              <button className="btn special-btn" onClick={logout}>Logout</button>
+            </li>
+          </>
+        )}
+      </ul>
     </nav>
-    
   );
 }
-
-
-
-{/* <nav className="navbar bg-body-transparent px-3">
-          <a className="navbar-brand" href="/#">Navbar</a>
-          <ul className="nav nav-pills">
-            <li className="nav-item">
-              <button className="btn">
-                <a className="nav-link" href="/create-recipe">create recipe</a>
-              </button>
-            </li>
-            <li className="nav-item">
-              <button className="btn">
-                <a className="nav-link" href="/profile">My profile</a>
-              </button>
-            </li>
-            <li className="nav-item">
-              <button className="btn">
-                <a className="nav-link" href="/login">login</a>
-              </button>
-            </li>
-            <li className="nav-item">
-              <button className="btn special-btn">
-                <a className="nav-link" href="/register">Register</a>
-              </button>
-            </li>
-          </ul>
-        </nav> */}
